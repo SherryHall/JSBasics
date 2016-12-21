dd4b02b6293555f3ebe813bea500ad159c32cc93
SherryHall <slhall222@gmail.com>
2016-12-21 16:49:13 -0500

    add code.js for level 2

---
+++ code.js
@@ -0,0 +1,22 @@
+// 1. Assign the message "Hello, World!" to a variable.
+var greeting = "Hello, World!";
+console.log (greeting);
+
+// 2. Assign a different string to a variable name of your choice.
+var answer = "Hello to you, too";
+console.log (answer);
+// 3. Assign a number to a variable.
+var myNum = 21;
+// 4. Use string concatenation to display the number from #3 in a string.
+var wishList = "Angel wishes he was " + myNum;
+// 5. Make an array of at least four of your favorite movies or books or bands.
+var myMovies = ["Star Wars", "Star Trek", "Indian Jones", "Live Free or Die Hard"];
+
+for (var i=0; i < myMovies.length; i++) {
+	console.log (myMovies[i]);
+}
+// 6. Make a object of information about yourself with at least four properties.
+var mySelf = {name : "Sherry", eye_color : "blue", hair_color : "brown", age : 23};
+console.log("My name is " + mySelf.name);
+	
+	
